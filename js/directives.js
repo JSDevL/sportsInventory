1d6d6e1475073d3bec7f7379015053b4ccdc674a
JSDevL <arfath.7920@gmail.com>
2016-08-06 14:16:02 +0530

    First Push

---
+++ js/directives.js
@@ -0,0 +1,26 @@
+invApp.directive('transactionForm',function(){
+    return {
+        restrict : "E",
+        templateUrl : "js/templates/transaction-form.html",
+        replace : true,
+        scope : {
+            year : "@",
+            itemKey : "@",
+            addInv : "&",
+            addDisp : "&",
+            printDiv : "&",
+            itemName : "@"
+        }
+    };
+});
+
+invApp.directive('transactionDetails',function(){
+    return {
+        restrict : "E",
+        templateUrl : "js/templates/transaction-details.html",
+        replace : true,
+        scope : {
+            transaction : "="
+        }
+    };
+});
